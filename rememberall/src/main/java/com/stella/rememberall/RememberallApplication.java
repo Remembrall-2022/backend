ff92b9251646b337918d8ef4e86fed7203cc88e9
mingadinga <minpearl0826@gmail.com>
2022-08-05 13:53:51 +0900

    setting:> set spring boot init #1

---
+++ rememberall/src/main/java/com/stella/rememberall/RememberallApplication.java
@@ -0,0 +1,13 @@
+package com.stella.rememberall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberallApplication.class, args);
+	}
+
+}
---
+++ rememberall/src/test/java/com/stella/rememberall/RememberallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stella.rememberall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
