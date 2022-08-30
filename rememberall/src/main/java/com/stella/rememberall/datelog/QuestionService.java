d180c777628287edf59839d8d16d58f294a0284c
NamJihyun99 <namjihyun9936@gmail.com>
2022-08-30 16:47:38 +0900

    feat:> develop readRandomId method in QuestionServie /#34

---
+++ rememberall/src/main/java/com/stella/rememberall/datelog/QuestionService.java
@@ -0,0 +1,27 @@
+package com.stella.rememberall.datelog;
+
+import com.stella.rememberall.datelog.domain.Question;
+import com.stella.rememberall.datelog.exception.QuestionExCode;
+import com.stella.rememberall.datelog.exception.QuestionException;
+import com.stella.rememberall.datelog.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    @Transactional
+    public Question readRandomQuestion() {
+        Long count = questionRepository.countQuestions();
+        Long randomId = new Random().nextLong()%count+1;
+
+        return questionRepository.findById(randomId)
+                .orElseThrow(() -> new QuestionException(QuestionExCode.QUESTION_NOT_FOUND));
+    }
+}
--- rememberall/src/main/java/com/stella/rememberall/datelog/repository/QuestionRepository.java
+++ rememberall/src/main/java/com/stella/rememberall/datelog/repository/QuestionRepository.java
@@ -3,10 +3,14 @@ package com.stella.rememberall.datelog.repository;
 import com.stella.rememberall.datelog.domain.Question;
 import com.stella.rememberall.datelog.domain.QuestionCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     List<Question> findByQuestionCategory(QuestionCategory category);
+
+    @Query("SELECT count(q.id) FROM Question q")
+    Long countQuestions();
 }
