e08d702f7e0c721ab3b9235c479af9a0e72c1cfa
NamJihyun99 <87467801+NamJihyun99@users.noreply.github.com>
2022-09-06 01:39:59 +0900

    Revert "[fix] - 출석 리워드 예외 처리"

--- rememberall/src/main/java/com/stella/rememberall/dongdong/Dongdong.java
+++ rememberall/src/main/java/com/stella/rememberall/dongdong/Dongdong.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "dongdong")
@@ -27,13 +26,9 @@ public class Dongdong {
     @Setter
     private Long exp;
 
-    @Setter
-    private LocalDate currentDateOfAttendance;
-
     public Dongdong (User user) {
         this.user = user;
         this.point = 0L;
         this.exp = 0L;
-        this.currentDateOfAttendance = user.getCreatedDate().toLocalDate().minusDays(1);
     }
 }
