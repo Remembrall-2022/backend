3becb94bb5d05c837fb0cccc88b14b48f558a3fa
Mingadinga <minpearl0826@gmail.com>
2022-09-14 01:21:03 +0900

    develop:> develop checkDateDuplicateInTripLog #87

---
+++ rememberall/src/main/java/com/stella/rememberall/datelog/dto/DateExistResponseDto.java
@@ -0,0 +1,12 @@
+package com.stella.rememberall.datelog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DateExistResponseDto {
+    Boolean isExists;
+}
