4e9c76394f3527605950cd255709daca526698b2
NamJihyun99 <namjihyun9936@gmail.com>
2023-08-16 17:00:46 +0900

    feat:>Object for mapping/#96

--- rememberall/src/main/java/com/stella/rememberall/datelog/domain/QuestionCategoryName.java
+++ rememberall/src/main/java/com/stella/rememberall/datelog/domain/QuestionCategoryName.java
@@ -1,6 +1,5 @@
 package com.stella.rememberall.datelog.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
---
+++ rememberall/src/main/java/com/stella/rememberall/question/QuestionData.java
@@ -0,0 +1,9 @@
+package com.stella.rememberall.question;
+
+import lombok.Data;
+
+@Data
+public class QuestionData {
+    private String topic;
+    private String questionCategory;
+}
