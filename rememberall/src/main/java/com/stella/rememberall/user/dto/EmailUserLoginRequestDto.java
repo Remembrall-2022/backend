fbedc4e8d72b7ac505c2dbf1039a0edeac0db9bd
mingadinga <minpearl0826@gmail.com>
2023-11-18 00:52:50 +0900

    fix:> changed the message of email validation error

--- rememberall/src/main/java/com/stella/rememberall/user/dto/EmailUserLoginRequestDto.java
+++ rememberall/src/main/java/com/stella/rememberall/user/dto/EmailUserLoginRequestDto.java
@@ -11,6 +11,6 @@ import javax.validation.constraints.NotEmpty;
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class EmailUserLoginRequestDto {
-    @Email private String email;
+    @Email(message = "email 형식이 유효하지 않습니다.") private String email;
     @NotEmpty(message = "회원의 password는 빈값일 수 없습니다.") private String password;
 }
